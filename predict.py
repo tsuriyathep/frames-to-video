80cc9542fe9ed477f03248ee8a204dc8c202f64e
Tony Suriyathep <tony@spinplaygames.com>
2024-03-20 09:26:57 -0700

    Using temp path instead of tmp.

--- predict.py
+++ predict.py
@@ -4,6 +4,7 @@ import os
 import zipfile
 import requests
 import shutil
+import tempfile
 
 class Predictor(BasePredictor):
     def predict(self,
@@ -12,15 +13,15 @@ class Predictor(BasePredictor):
                 fps: float = Input(description="Number of frames per second of video", default=24.0, ge=1.0),
     ) -> Path:
         """Run ffmpeg to combine frames into a video"""
-        os.makedirs("/tmp/frames", exist_ok=True)
+        temp_folder_path = tempfile.mkdtemp()
 
         if frames_zip is not None:
             with zipfile.ZipFile(frames_zip, 'r') as zip_ref:
-                zip_ref.extractall("/tmp/frames")
+                zip_ref.extractall(f"{temp_folder_path}/frames")
 
-            frame_files = sorted(os.listdir("/tmp/frames"))
+            frame_files = sorted(os.listdir(f"/{temp_folder_path}/frames"))
             for i, frame_file in enumerate(frame_files):
-                os.rename(os.path.join("/tmp/frames", frame_file), f"/tmp/frames/out{i:03d}.png")
+                os.rename(os.path.join(f"{temp_folder_path}/frames", frame_file), f"{temp_folder_path}/frames/out{i:03d}.png")
 
         elif frames_urls is not None:
             url_list = frames_urls.split('\n')
@@ -28,14 +29,14 @@ class Predictor(BasePredictor):
                 response = requests.get(url, stream=True)
                 response.raise_for_status()
 
-                with open(f"/tmp/frames/out{i:03d}.png", 'wb') as out_file:
+                with open(f"{temp_folder_path}/frames/out{i:03d}.png", 'wb') as out_file:
                     shutil.copyfileobj(response.raw, out_file)
 
         else:
             raise ValueError("Must provide either frames_zip or frames_urls")
 
-        video = "/tmp/out.mp4"
-        command = f"ffmpeg -r {fps} -i /tmp/frames/out%03d.png -c:v libx264 -vf 'fps={fps},format=yuv420p' {video}"
+        video = f"{temp_folder_path}/out.mp4"
+        command = f"ffmpeg -r {fps} -i {temp_folder_path}/frames/out%03d.png -c:v libx264 -vf 'fps={fps},format=yuv420p' {video}"
 
         subprocess.run(command, shell=True, check=True)
 
